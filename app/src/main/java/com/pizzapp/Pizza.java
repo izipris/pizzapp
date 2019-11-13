26c33ebb9263692d8b5e82fc3e1cb9ad69a1cbc6
aviad dudkevich <aviad.dudkevich@mail.huji.ac.il>
2019-11-13 11:11:18 +0200

    more fields in pizza

--- app/src/main/java/com/pizzapp/Pizza.java
+++ app/src/main/java/com/pizzapp/Pizza.java
@@ -4,5 +4,8 @@ import java.util.ArrayList;
 
 class Pizza {
     private int id;
+    private int size;
+    private int thick;
+    private int cheeseType;
     private ArrayList<Topping> parts;
 }
