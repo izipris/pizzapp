1766033e0f5b38a5c18d72cebf8f25c9e7cfd6df
aviad dudkevich <aviad.dudkevich@mail.huji.ac.il>
2019-11-13 11:06:58 +0200

    new class

---
+++ app/src/main/java/com/pizzapp/Pizza.java
@@ -0,0 +1,8 @@
+package com.pizzapp;
+
+import java.util.ArrayList;
+
+class Pizza {
+    private int id;
+    private ArrayList<Topping> parts;
+}
---
+++ app/src/main/java/com/pizzapp/Topping.java
@@ -0,0 +1,4 @@
+package com.pizzapp;
+
+class Topping {
+}
