69278a0d77f952640dd11001810ee04708754b88
elad-m <musbae7@gmail.com>
2019-11-15 15:43:37 +0200

    just connecting between main activity to order_summary_activity to enable testing

--- app/src/main/java/com/pizzapp/OrderSummary.java
+++ app/src/main/java/com/pizzapp/OrderSummary.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.pizzapp.databinding.ActivityOrderSummaryBinding;
 
@@ -13,15 +14,18 @@ import static com.pizzapp.BR.order;
 
 public class OrderSummary extends AppCompatActivity {
 
+    Order order;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_summary);
-        ActivityOrderSummaryBinding binding = DataBindingUtil.setContentView(
-                this, R.layout.activity_order_summary);
-        Order order = new Order(123);  // todo not sure what this does
-        binding.setOrder(order);
-
+//        ActivityOrderSummaryBinding binding = DataBindingUtil.setContentView(
+//                this, R.layout.activity_order_summary);
+//        binding.setOrder(order);
+//        Part part = new Part(4, "pineapple");
+        order = new Order(123);  // todo not sure what this does
+        TextView textView = findViewById(R.id.order_text);
+        textView.setText(order.toString());
     }
 
 
