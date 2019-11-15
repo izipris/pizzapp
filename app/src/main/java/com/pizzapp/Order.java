34098b0ae2eddb45cb4c342653988650b9a26ebd
elad-m <musbae7@gmail.com>
2019-11-15 11:44:10 +0200

    added order summary activity

---
+++ app/src/main/java/com/pizzapp/Order.java
@@ -0,0 +1,43 @@
+package com.pizzapp;
+
+public class Order {
+
+    public Order(int id) {
+        this.id = id;
+        this.price = 0;
+    }
+
+    private class Pizza{
+
+    }
+
+    private int id;
+    private Pizza pizza;
+    private int price;
+
+
+
+    public Pizza getPizza() {
+        return pizza;
+    }
+    public void setPizza(Pizza pizza) {
+        this.pizza = pizza;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+}
