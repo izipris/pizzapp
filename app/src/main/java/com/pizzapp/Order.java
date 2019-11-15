98533cdb2375e580453a659fe3903ca885fea3b9
elad-m <musbae7@gmail.com>
2019-11-15 15:43:37 +0200

    just connecting between main activity to order_summary_activity to enable testing

--- app/src/main/java/com/pizzapp/Order.java
+++ app/src/main/java/com/pizzapp/Order.java
@@ -1,14 +1,25 @@
 
 package com.pizzapp;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Order {
 
     public Order(int mId) {
         this.mId = mId;
+        this.mPizzas = new ArrayList<>();
         this.mPizzas.add(new Pizza(1));
         this.mPrice = 0.0;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "mId=" + mId +
+                ", mPizzas=" + mPizzas +
+                ", mPrice=" + mPrice +
+                '}';
     }
 
     private int mId;
