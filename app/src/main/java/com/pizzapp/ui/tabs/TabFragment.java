da69b3f5ffe0afcddedb68fcbe53d649f679579b
idzipris <idzipris@ebay.com>
2019-11-17 00:10:22 +0200

    integrated the data from the database into the tabs

--- app/src/main/java/com/pizzapp/ui/tabs/TabFragment.java
+++ app/src/main/java/com/pizzapp/ui/tabs/TabFragment.java
@@ -10,6 +10,13 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.pizzapp.R;
+import com.pizzapp.model.Database;
+import com.pizzapp.utilities.IO;
+
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class TabFragment extends Fragment {
 
@@ -39,16 +46,25 @@ public class TabFragment extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        InputStream jsonDatabaseInputStream = getResources().openRawResource(R.raw.database);
+        Database jsonDb = new Database();
+        try {
+            jsonDb = IO.jsonFileToDatabase(jsonDatabaseInputStream);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         textView = (TextView) view.findViewById(R.id.textView);
-        switch (position){
+        switch (position) {
             case 0:
-                textView.setText("Choose Size");
+                textView.setText(jsonDb.getSizes().get(0).getName() + " / " + jsonDb.getSizes().get(1).getName() + " / " + jsonDb.getSizes().get(2).getName());
                 break;
             case 1:
                 textView.setText("My Pizza");
                 break;
             case 2:
-                textView.setText("Choose Crust");
+                textView.setText(jsonDb.getCrusts().get(0).getName() + " / " + jsonDb.getCrusts().get(1).getName() + " / " + jsonDb.getCrusts().get(2).getName());
                 break;
         }
 
