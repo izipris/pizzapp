f4c4beb77d619b9f54dd33b75adb7c3b5640be2f
elad-m <musbae7@gmail.com>
2019-11-17 20:04:48 +0200

    adding buttons at the bottom

--- app/src/main/java/com/pizzapp/OrderSummary.java
+++ app/src/main/java/com/pizzapp/OrderSummary.java
@@ -1,16 +1,14 @@
 package com.pizzapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.databinding.DataBindingUtil;
+//import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
-import android.widget.TextView;
+//import android.widget.TextView;
+//
+//import org.json.JSONObject;
+//
 
-import com.pizzapp.databinding.ActivityOrderSummaryBinding;
-
-import org.json.JSONObject;
-
-import static com.pizzapp.BR.order;
 
 public class OrderSummary extends AppCompatActivity {
 
@@ -21,11 +19,11 @@ public class OrderSummary extends AppCompatActivity {
         setContentView(R.layout.activity_order_summary);
 //        ActivityOrderSummaryBinding binding = DataBindingUtil.setContentView(
 //                this, R.layout.activity_order_summary);
-//        binding.setOrder(order);
+//        binding.setOrder(order);// todo not sure what this does
 //        Part part = new Part(4, "pineapple");
-        order = new Order(123);  // todo not sure what this does
-        TextView textView = findViewById(R.id.order_text);
-        textView.setText(order.toString());
+//        order = new Order(123);
+//        TextView textView = findViewById(R.id.order_text);
+//        textView.setText(order.toString());
     }
 
 
