d7e4061c537ca975d0b836109ef2bd67bffe5a3e
aviad dudkevich <aviad.dudkevich@mail.huji.ac.il>
2019-11-18 11:20:27 +0200

    add pizza object

---
+++ app/src/main/java/com/pizzapp/model/pizza/pizza.java
@@ -0,0 +1,13 @@
+package com.pizzapp.model.pizza;
+
+import java.util.List;
+
+/**
+ * This class represent a pizza in an order.
+ */
+public class pizza {
+    private Size size;
+    private Crust crust;
+    //private CheeseType cheeseType;
+    private List<Topping> parts;
+}
