7327cc22012da44f5648f622b831e314bea0bfd6
aviad dudkevich <aviad.dudkevich@mail.huji.ac.il>
2019-11-18 13:14:34 +0200

    finish pizza object

--- app/src/main/java/com/pizzapp/model/pizza/Pizza.java
+++ app/src/main/java/com/pizzapp/model/pizza/Pizza.java
@@ -1,30 +1,29 @@
 package com.pizzapp.model.pizza;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
-import java.util.Vector;
 
 /**
  * This class represent a pizza in an order.
  */
 public class Pizza {
+    private static final int DEFAULT_NUMBER_OF_SLICES = 1;
+    private static final String OUT_OF_BOUNDS_MSG = "There no pizza part in the given index.";
+
+    private int numberOfSlices;
     private Size size;
     private Crust crust;
     private CheeseType cheeseType;
-    private List<PizzaPart> parts;
+    private PizzaPart[] parts;
     private double price;
 
-    public Pizza(Size size, Crust crust, CheeseType cheeseType){
+    public Pizza(int numberOfSlices, Size size, Crust crust, CheeseType cheeseType){
+        this.numberOfSlices = numberOfSlices;
         this.size = size;
         this.crust = crust;
         this.cheeseType = cheeseType;
-        parts = new Vector<>();
-        parts.add(new PizzaPart());
+        parts = new PizzaPart[numberOfSlices];
+        for(int i = 0; i < numberOfSlices; ++i) {
+            parts[i] = new PizzaPart();
+        }
         calculatePrice();
     }
 
@@ -33,6 +32,7 @@ public class Pizza {
      * @param pizza another pizza.
      */
     public Pizza(Pizza pizza){
+        this.numberOfSlices = pizza.numberOfSlices;
         this.size = pizza.size;
         this.crust = pizza.crust;
         this.cheeseType = pizza.cheeseType;
@@ -40,11 +40,15 @@ public class Pizza {
         this.price = pizza.price;
     }
 
+    public Pizza(Size size, Crust crust, CheeseType cheeseType){
+        this(DEFAULT_NUMBER_OF_SLICES, size, crust, cheeseType);
+    }
+
     private void calculatePrice(){
         price = size.getPrice() + crust.getPrice() + cheeseType.getPrice();
         for (PizzaPart part: parts){
             if (part.isHasTopping()){
-                price += part.getTopping().getPrice() / (double) parts.size();
+                price += part.getTopping().getPrice() / (double) numberOfSlices;
             }
         }
     }
@@ -75,5 +79,12 @@ public class Pizza {
 
     public void setCheeseType(CheeseType cheeseType) {
         this.cheeseType = cheeseType;
+    }
+
+    public PizzaPart getPizzaPart(int index) throws IndexOutOfBoundsException{
+        if (index < 0 || index > numberOfSlices){
+            throw new IndexOutOfBoundsException(OUT_OF_BOUNDS_MSG);
+        }
+        return parts[index];
     }
 }
