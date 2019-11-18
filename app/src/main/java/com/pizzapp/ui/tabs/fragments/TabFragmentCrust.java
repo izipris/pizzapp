69b59f03ebbdf7a7de8889b1a726482049a30619
idzipris <idzipris@ebay.com>
2019-11-18 12:23:52 +0200

    added the different crusts to the crust tab

--- app/src/main/java/com/pizzapp/ui/tabs/fragments/TabFragmentCrust.java
+++ app/src/main/java/com/pizzapp/ui/tabs/fragments/TabFragmentCrust.java
@@ -4,14 +4,45 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.pizzapp.R;
+import com.pizzapp.model.Database;
+import com.pizzapp.model.pizza.Crust;
+import com.pizzapp.utilities.IO;
 
 public class TabFragmentCrust extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.tab_fragment_crust, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        final int DB_CRUST_THICK = 2;
+        final int DB_CRUST_REGULAR = 1;
+        final int DB_CRUST_THIN = 0;
+        TextView doughThickTextView = view.findViewById(R.id.doughThickTextView);
+        TextView doughRegularTextView = view.findViewById(R.id.doughRegularTextView);
+        TextView doughThinTextView = view.findViewById(R.id.doughThinTextView);
+        Database database = IO.getDatabaseFromInputStream(getResources().openRawResource(R.raw.database));
+
+        Crust thinCrust = database.getCrusts().get(DB_CRUST_THIN);
+        Crust regularCrust = database.getCrusts().get(DB_CRUST_REGULAR);
+        Crust thickCrust = database.getCrusts().get(DB_CRUST_THICK);
+
+        doughThickTextView.setText(getDoughTitle(thickCrust));
+        doughRegularTextView.setText(getDoughTitle(regularCrust));
+        doughThinTextView.setText(getDoughTitle(thinCrust));
+    }
+
+    private String getDoughTitle(Crust crust) {
+        return crust.getPrice() > 0 ? crust.getName() + " + " +
+                String.format("%.2f", crust.getPrice()) +
+                getString(R.string.currency_symbol) : crust.getName();
     }
 }
