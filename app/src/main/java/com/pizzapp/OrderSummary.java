9967a91d6a0826f32ac367e96bbb8b2a21729893
elad-m <musbae7@gmail.com>
2019-11-18 13:38:23 +0200

    Started working on reading from a json file into the order text view

--- app/src/main/java/com/pizzapp/OrderSummary.java
+++ app/src/main/java/com/pizzapp/OrderSummary.java
@@ -3,27 +3,69 @@ package com.pizzapp;
 import androidx.appcompat.app.AppCompatActivity;
 //import androidx.databinding.DataBindingUtil;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static java.nio.file.Files.readAllBytes;
+import static java.nio.file.Paths.get;
+
 //import android.widget.TextView;
-//
 //import org.json.JSONObject;
-//
 
 
 public class OrderSummary extends AppCompatActivity {
 
-
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_summary);
-//        ActivityOrderSummaryBinding binding = DataBindingUtil.setContentView(
-//                this, R.layout.activity_order_summary);
-//        binding.setOrder(order);// todo not sure what this does
-//        Part part = new Part(4, "pineapple");
-//        order = new Order(123);
-//        TextView textView = findViewById(R.id.order_text);
-//        textView.setText(order.toString());
+
+    }
+
+    public String byteBufferToString(byte[] buffer){
+        try{
+            String order_as_json = new String(buffer, "UTF-8");
+            return order_as_json;
+        } catch (UnsupportedEncodingException ex){
+            ex.printStackTrace();
+            return null;
+        }
+    }
+
+
+    public String loadJSONFromAsset(Context context) {
+        try {
+            InputStream is = context.getAssets().open("order.json");
+            byte[] buffer = new byte[is.available()]; // todo: they say not to use but cannot find alternative to go with android 15 and up
+            int bytes_read = is.read(buffer);
+            if(bytes_read == 0 || buffer.length == 0){
+                Log.d(LOG_TAG, "empty or zero bytes or");
+                return null;  // todo
+            }
+            is.close();
+            return byteBufferToString(buffer);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        } catch (NullPointerException ex){
+            ex.printStackTrace();
+            return null;
+        }
     }
 
 
