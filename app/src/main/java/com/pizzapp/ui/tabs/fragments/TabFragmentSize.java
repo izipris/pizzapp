e25b81c456048cafed6e57d7fe330de76718bbce
aviad dudkevich <aviad.dudkevich@mail.huji.ac.il>
2019-11-19 21:41:35 +0200

    setup text from DB

--- app/src/main/java/com/pizzapp/ui/tabs/fragments/TabFragmentSize.java
+++ app/src/main/java/com/pizzapp/ui/tabs/fragments/TabFragmentSize.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -18,10 +19,14 @@ import com.pizzapp.utilities.IO;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
 
 public class TabFragmentSize extends Fragment {
 
     private Size chosenSize;
+    static private final int DB_SIZE_M = 0;
+    static private final int DB_SIZE_L = 1;
+    static private final int DB_SIZE_XL = 2;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -31,6 +36,11 @@ public class TabFragmentSize extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        Database database = IO.getDatabaseFromInputStream(getResources().openRawResource(R.raw.database));
+        setupText(view, database);
+
+        chosenSize = database.getSizes().get(DB_SIZE_M);
 
         ImageButton mediumPizzaButton = view.findViewById(R.id.medium_pizza_image);
         ImageButton largePizzaButton = view.findViewById(R.id.large_pizza_image);
@@ -44,10 +54,34 @@ public class TabFragmentSize extends Fragment {
                 }
             });
         }
+    }
 
-        Database database = IO.getDatabaseFromInputStream(getResources().openRawResource(R.raw.database));
-
-        chosenSize = database.getSizes().get(0);
+    private void setupText(@NonNull View view, Database database){
+        Size currentSize;
+        for(int i = 0; i < database.getSizes().size(); ++i)
+        {
+            currentSize = database.getSizes().get(i);
+            switch (i){
+                case DB_SIZE_M:
+                    ((TextView) view.findViewById(R.id.M)).setText(currentSize.getName());
+                    ((TextView) view.findViewById(R.id.medium_pizza_price)).setText(
+                            String.format(Locale.getDefault(),"%.2f", currentSize.getPrice()) +
+                            getString(R.string.currency_symbol) );
+                    break;
+                case DB_SIZE_L:
+                    ((TextView) view.findViewById(R.id.L)).setText(currentSize.getName());
+                    ((TextView) view.findViewById(R.id.large_pizza_price)).setText(
+                            String.format(Locale.getDefault(),"%.2f", currentSize.getPrice()) +
+                                    getString(R.string.currency_symbol) );
+                    break;
+                case DB_SIZE_XL:
+                    ((TextView) view.findViewById(R.id.XL)).setText(currentSize.getName());
+                    ((TextView) view.findViewById(R.id.extra_large_pizza_price)).setText(
+                            String.format(Locale.getDefault(),"%.2f", currentSize.getPrice()) +
+                                    getString(R.string.currency_symbol) );
+                    break;
+            }
+        }
 
     }
 
@@ -59,4 +93,5 @@ public class TabFragmentSize extends Fragment {
             }
         }
     }
+
 }
