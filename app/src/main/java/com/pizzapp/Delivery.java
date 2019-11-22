f21e694b922b9f57a54b34a36725740fe8e66271
elad-m <musbae7@gmail.com>
2019-11-22 15:45:41 +0200

    Added an overly simplified activities following pressing the delivery and pickup buttons for more 'flow'

---
+++ app/src/main/java/com/pizzapp/Delivery.java
@@ -0,0 +1,16 @@
+package com.pizzapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Delivery extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delivery);
+
+
+    }
+}
