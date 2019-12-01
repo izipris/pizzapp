7dc5b206c10dcb6b4a557ba77e20484702643b8f
aviad dudkevich <aviad.dudkevich@mail.huji.ac.il>
2019-12-01 20:01:54 +0200

    reduce database creation

--- app/src/main/java/com/pizzapp/MainActivity.java
+++ app/src/main/java/com/pizzapp/MainActivity.java
@@ -8,10 +8,9 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.viewpager.widget.ViewPager;
 
 import com.google.android.material.tabs.TabLayout;
+import com.pizzapp.model.Database;
 import com.pizzapp.model.Order;
-import com.pizzapp.model.pizza.Crust;
 import com.pizzapp.model.pizza.Pizza;
-import com.pizzapp.model.pizza.Size;
 import com.pizzapp.ui.tabs.TabAdapter;
 import com.pizzapp.ui.tabs.fragments.TabFragmentCrust;
 import com.pizzapp.ui.tabs.fragments.TabFragmentMain;
@@ -23,13 +22,13 @@ import java.io.Serializable;
 public class MainActivity extends AppCompatActivity implements Serializable {
 
     private static final int MAIN_FRAGMENT_INDEX = 1;
-    private static final int DEFAULT_NUMBER_OF_SLICES = 4;
     private Toolbar toolbar;
     private TabAdapter tabAdapter;
     private TabLayout tabLayout;
     private ViewPager viewPager;
     public Pizza pizza;
     public Order order;
+    public Database database;
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
     @Override
@@ -37,38 +36,28 @@ public class MainActivity extends AppCompatActivity implements Serializable {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d(LOG_TAG, "onCreate");
-        initiatePizza();
+
+        extractExtras();
+
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        database = IO.getDatabaseFromInputStream(getResources().openRawResource(R.raw.database));
         viewPager = findViewById(R.id.viewpager);
         tabLayout = findViewById(R.id.tabs);
         initTabsLayout(viewPager, tabLayout);
     }
 
-    private void initiatePizza() {
+    private void extractExtras() {
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
-            extractExtras(extras);
-        } else {
-            createPizzaOrder();
+            pizza = (Pizza) extras.getSerializable("pizza");
+            order = (Order) extras.getSerializable("order");
+            viewPager = findViewById(R.id.viewpager);
+            viewPager.setCurrentItem(MAIN_FRAGMENT_INDEX);
+
         }
-    }
-
-    private void createPizzaOrder() {
-        Size defaultSize = IO.getDatabaseFromInputStream(getResources().openRawResource(R.raw.database)).getSizes().get(0);
-        Crust defaultCrust = IO.getDatabaseFromInputStream(getResources().openRawResource(R.raw.database)).getCrusts().get(0);
-        pizza = new Pizza(DEFAULT_NUMBER_OF_SLICES, defaultSize, defaultCrust);
-        order = new Order(0);
-        order.addPizza(pizza);
-    }
-
-    private void extractExtras(Bundle extras) {
-        pizza = (Pizza) extras.getSerializable("pizza");
-        order = (Order) extras.getSerializable("order");
-        viewPager = findViewById(R.id.viewpager);
-        viewPager.setCurrentItem(MAIN_FRAGMENT_INDEX);
     }
 
     private void initTabsLayout(ViewPager viewPager, TabLayout tabLayout) {
@@ -81,10 +70,6 @@ public class MainActivity extends AppCompatActivity implements Serializable {
         viewPager.setCurrentItem(MAIN_FRAGMENT_INDEX, false);
         viewPager.setOffscreenPageLimit(viewPagerTabsLimit);
         tabLayout.setupWithViewPager(viewPager);
-    }
-
-    public Pizza getPizza() {
-        return pizza;
     }
 
     @Override
