8f67d1565c1d3d73f6de58983fc7b4122642a775
elad-m <musbae7@gmail.com>
2019-12-08 14:24:50 +0200

    Does not show the rows programmatically.
    I wrote code to generate a table row to show one pizza (not even drawing
     the toppings. The first line that do appear is hard coded in the xml to
     get a reference.
    
     This commit is to save and show the work so far, in case it is easily
     fixable.

--- app/src/main/java/com/pizzapp/OrderSummary.java
+++ app/src/main/java/com/pizzapp/OrderSummary.java
@@ -1,25 +1,32 @@
 package com.pizzapp;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
+import android.view.LayoutInflater;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.NavUtils;
 
 import com.pizzapp.model.Order;
 import com.pizzapp.model.pizza.Pizza;
 import com.pizzapp.model.pizza.PizzaPart;
 import com.pizzapp.model.pizza.Topping;
+import com.pizzapp.utilities.DoesNotExist;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -28,10 +35,13 @@ import java.util.List;
 public class OrderSummary extends AppCompatActivity {
 
     private static final String LOG_TAG = OrderSummary.class.getSimpleName();
-    private LinearLayout mPizzaLayout;
+    LayoutInflater inflater;
+    //    private LinearLayout mPizzaLayout;
+    private TableLayout mPizzaLayout;
     private Button mDelivery;
     private Button mPickup;
     private Order finalOrder;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +50,7 @@ public class OrderSummary extends AppCompatActivity {
 
         initializeDataMembers();
         Intent intent = getIntent();
-
+        // todo: change heading here form hello to Order Summary
         finalOrder = (Order) intent.getSerializableExtra("order");
 
         if (finalOrder != null) {
@@ -50,7 +60,6 @@ public class OrderSummary extends AppCompatActivity {
         }
     }
 
-
     public boolean onOptionsItemSelected(MenuItem item) {
         Intent intent = NavUtils.getParentActivityIntent(this);
         intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
@@ -59,7 +68,8 @@ public class OrderSummary extends AppCompatActivity {
     }
 
     private void initializeDataMembers() {
-        mPizzaLayout = findViewById(R.id.pizza_text_layout);
+        inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        mPizzaLayout = findViewById(R.id.table_of_pizzas);
         mPickup = findViewById(R.id.pickup_button);
         mDelivery = findViewById(R.id.delivery_button);
     }
@@ -72,23 +82,156 @@ public class OrderSummary extends AppCompatActivity {
     private void viewOrder(Order testOrder) {
         try {
             for (int i = 0; i < testOrder.getNumberOfPizzas(); i++) {
-                List<TextView> pizzaViews = getPizzaViews(testOrder.getPizza(i));
-                for (TextView view : pizzaViews)
-                    mPizzaLayout.addView(view);
+                Log.d(LOG_TAG, "There is a pizza to order");
+                TableRow pizzaRow = getPizzaRow(testOrder.getPizza(i));
+                mPizzaLayout.addView(pizzaRow);
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    private TextView getSizeView(Pizza pizza) {
-        TextView pizzaSizeView = getAnonymousTextView();
-        pizzaSizeView.setText(pizza.getSize().getCaption() + "Pizza | "
-                + pizza.getCrust().getName() + " Crust");
-        pizzaSizeView.setTextSize(17);
-        pizzaSizeView.setPadding(5, 5, 5, 5);
-        pizzaSizeView.setVisibility(View.VISIBLE);
-        return pizzaSizeView;
+    private TableRow getPizzaRow(Pizza pizza) {
+        TableRow row = getTableRow();
+        RelativeLayout pizzaDrawing = getPizzaDrawingLayout(pizza); // table layout of two rows. add toppings
+        LinearLayout pizzaText = getPizzaTextLayout();
+        pizzaText.addView(getPizzaHeadlineLayout());
+        addPizzaDetails(pizza, pizzaText);
+
+        row.addView(pizzaDrawing);
+        row.addView(pizzaText);
+        return row;
+    }
+
+    private RelativeLayout getPizzaDrawingLayout(Pizza pizza) {
+        RelativeLayout pizzaDrawing = new RelativeLayout(this);
+        pizzaDrawing.setLayoutParams(new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.MATCH_PARENT,
+                RelativeLayout.LayoutParams.MATCH_PARENT));
+
+        TableLayout onlyPizza = getPizzaTableLayout();
+        for (int part_i = 0; part_i < 4; part_i += 2){
+            TableRow rowOfTwoParts = getTableRow();
+            FrameLayout drawnPartLeft = drawPart(pizza.getPizzaPart(part_i));
+            rowOfTwoParts.addView(drawnPartLeft);
+            FrameLayout drawnPartRight = drawPart(pizza.getPizzaPart(part_i + 1));
+            rowOfTwoParts.addView(drawnPartRight);
+            onlyPizza.addView(rowOfTwoParts);
+        }
+        addChangeButton(pizzaDrawing);
+        return pizzaDrawing;
+    }
+
+    private void addChangeButton(RelativeLayout pizzaDrawing) {
+        Button myButton = (Button)inflater.inflate(R.layout.change_button_layout, null);
+        pizzaDrawing.addView(myButton);
+        // todo: NOT CRUCIAL: add here a specific on click for a specific pizza to load in main
+    }
+
+
+    public FrameLayout drawPart(PizzaPart part){
+        try{
+            FrameLayout frame = getPizzaPartFrameLayout();
+            ImageView partImage = getPizzaPartImage(part.getId());
+            // todo: add the topping on top of the image
+            frame.addView(partImage);
+            return frame;
+        } catch (Exception e) {
+            e.printStackTrace();
+            e.getCause();
+            return null;
+        }
+    }
+
+    private ImageView getPizzaPartImage(int part_i) throws DoesNotExist{
+        ImageView image = new ImageView(this);
+        image.setImageResource(R.drawable.ic_pizza_slice_color);
+        FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(R.dimen.recieptPizzaSize,
+                R.dimen.recieptPizzaSize);
+        switch (part_i){
+            case 0:
+                params.gravity = Gravity.BOTTOM|Gravity.END;
+                image.setLayoutParams(params);
+                image.setRotation(270);
+                break;
+            case 1:
+                params.gravity = Gravity.BOTTOM|Gravity.START;
+                image.setLayoutParams(params);
+                image.setRotation(0);
+                break;
+            case 2:
+                params.gravity = Gravity.TOP|Gravity.END;
+                image.setLayoutParams(params);
+                image.setRotation(180);
+                break;
+            case 3:
+                params.gravity = Gravity.TOP|Gravity.START;
+                image.setLayoutParams(params);
+                image.setRotation(90);
+                break;
+            default:
+                throw new DoesNotExist();
+        }
+        return image;
+    }
+
+
+    public FrameLayout getPizzaPartFrameLayout(){
+        FrameLayout frame = new FrameLayout(this);
+        FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(
+                R.dimen.recieptPizzaSize,
+                R.dimen.recieptPizzaSize);
+        params.gravity = Gravity.CENTER;
+        frame.setLayoutParams(params);
+        frame.setPadding(1,1,1,1);
+        return frame;
+    }
+
+    private TableLayout getPizzaTableLayout() {
+        TableLayout tableLayout = new TableLayout(this);
+        TableLayout.LayoutParams params = new TableLayout.LayoutParams(
+                                            TableLayout.LayoutParams.MATCH_PARENT,
+                                            TableLayout.LayoutParams.WRAP_CONTENT);
+        params.gravity = Gravity.CENTER;
+        tableLayout.setLayoutParams(params);
+        tableLayout.setPadding(15,15,15,15);
+        return  tableLayout;
+    }
+
+    private TableRow getTableRow() {
+        TableRow row = new TableRow(this);
+        row.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT,
+                TableRow.LayoutParams.MATCH_PARENT));
+        return row;
+    }
+
+
+    private LinearLayout getPizzaTextLayout() {
+        LinearLayout genLayout = new LinearLayout(this);
+        genLayout.setOrientation(LinearLayout.VERTICAL);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
+        params.setMargins(5, 5, 5, 5);
+        genLayout.setLayoutParams(params);
+        genLayout.setPadding(1, 1, 1, 1);
+        return genLayout;
+    }
+
+    private LinearLayout getPizzaHeadlineLayout() {
+        LinearLayout genLayout = new LinearLayout(this);
+        genLayout.setOrientation(LinearLayout.HORIZONTAL);
+        genLayout.setLayoutParams(new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+        return genLayout;
+    }
+
+    private void addPizzaDetails(Pizza pizza, LinearLayout pizzaText) {
+        int numOfParts = pizza.getNumberOfParts();
+        List<TextView> views = new LinkedList<>();
+        views.addAll(getAllPartsAndToppings(pizza, numOfParts));
+        views.add(getPizzaPriceView(pizza, numOfParts));
+        for (TextView view : views)
+            pizzaText.addView(view);
     }
 
 
@@ -147,17 +290,6 @@ public class OrderSummary extends AppCompatActivity {
         }
         return partsAndToppings;
     }
-
-
-    private List<TextView> getPizzaViews(Pizza pizza) {
-        int numOfParts = pizza.getNumberOfParts();
-        List<TextView> views = new LinkedList<>();
-        views.add(getSizeView(pizza));
-        views.addAll(getAllPartsAndToppings(pizza, numOfParts));
-        views.add(getPizzaPriceView(pizza, numOfParts));
-        return views;
-    }
-
 
     private TextView getPizzaPriceView(Pizza pizza, int numOfParts) {
         double price = pizza.getPrice();
