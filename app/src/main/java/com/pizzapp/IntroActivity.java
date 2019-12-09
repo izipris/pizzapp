a1b3a7da016111cf02283040fc646bb80820ec53
aviad dudkevich <aviad.dudkevich@mail.huji.ac.il>
2019-12-09 10:27:10 +0200

    fix welcome to text

--- app/src/main/java/com/pizzapp/IntroActivity.java
+++ app/src/main/java/com/pizzapp/IntroActivity.java
@@ -4,9 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 public class IntroActivity extends AppCompatActivity {
+    private static final String LOG_TAG = IntroActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
